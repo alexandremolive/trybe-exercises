0bece12bbb9c398cf25bdea719953b06d854420d
Alexandre Oliveira <alexandremolive@gmail.com>
2021-02-24 20:07:21 -0300

    resolucoes dos exercicios 1 e 2

---
+++ exercises/bloco_4/dia_1/scripts.js
@@ -0,0 +1,30 @@
+// PRIMEIRO 
+let a = 34;
+let b = 56;
+let resultado;
+
+resultado = a + b;
+console.log(resultado);
+
+resultado = a - b;
+console.log(resultado);
+
+resultado = a * b;
+console.log(resultado);
+
+resultado = a / b;
+console.log(resultado);
+
+resultado = a % b;
+console.log(resultado);
+
+//  SEGUNDO
+
+if (a > b) {
+    console.log('O maior é', a)
+} else {
+    console.log('O maior é', b)
+}
+
+//  TERCEIRO
+
