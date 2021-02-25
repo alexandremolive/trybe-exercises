2f1353dd3175e2e6c20e57b2823893b958227146
Alexandre Oliveira <alexandremolive@gmail.com>
2021-02-25 16:31:16 -0300

    mais alguns testes praticos

--- exercises/bloco_4/exemplos_praticos/scripts.js
+++ exercises/bloco_4/exemplos_praticos/scripts.js
@@ -1,29 +1,54 @@
-let nota = 150;
-let resultado;
+// DIA 1
 
-if (nota >= 80 && nota <= 100) {
-    resultado = 'Aprovada'
-}
-else if (nota >= 60 && nota < 80) {
-    resultado = 'Lista'
-}
-else if (nota < 60) {
-    resultado = 'Reprovado'
-}
+// let nota = 150;
+// let resultado;
 
-switch (resultado) {
-    case 'Aprovada':
-        console.log('Parabéns, você foi aprovado(a)!');
-        break;
+// if (nota >= 80 && nota <= 100) {
+//     resultado = 'Aprovada'
+// }
+// else if (nota >= 60 && nota < 80) {
+//     resultado = 'Lista'
+// }
+// else if (nota < 60) {
+//     resultado = 'Reprovado'
+// };
 
-    case 'Lista':
-        console.log('Você está na lista de espera');
-        break;
+// switch (resultado) {
+//     case 'Aprovada':
+//         console.log('Parabéns, você foi aprovado(a)!');
+//         break;
 
-    case 'Reprovado':
-        console.log('Você foi reprovado(a)');
-        break;
+//     case 'Lista':
+//         console.log('Você está na lista de espera');
+//         break;
 
-    default:
-        console.log('Nota não localizada')
+//     case 'Reprovado':
+//         console.log('Você foi reprovado(a)');
+//         break;
+
+//     default:
+//         console.log('Nota não localizada');
+// };
+
+// DIA 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato')
+
+// console.log(menu);
+
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for(let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i])
 }
