c0295ecde826142a9ae1caa9018c1f990ae1c795
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-01 22:36:37 -0300

    exercicios bloco 4 dia 4

---
+++ exercises/bloco_4/scripts.js
@@ -0,0 +1,136 @@
+// PRIMEIRO
+// let info = {
+
+//         personagem: "Margarida",
+//         origem: "Pato Donald",
+//         nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+
+// };
+// console.log('Bem vinda, ' + info.personagem)
+
+// // SEGUNDO
+// info.recorrente = 'Sim'
+// console.log(info)
+
+// //TERCEIRO
+// for (let key in info) {
+//     console.log(key)
+// }
+
+// //QUARTO
+// for (let key in info) {
+//     console.log(info[key])
+// }
+
+//QUINTO
+
+// info.personagem = 'Margarida e Tio Patinhas'
+// info.origem = 'Pato Donald e Christmas on Bear Mountain, Dells Four Color Comics #178'
+// info.nota = 'Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas'
+// info.recorrente = 'Ambos recorrentes'
+// console.table(info)
+
+//FUNÇÕES
+
+//PRIMEIRO
+// let checkPalindrome
+
+// function palindrome(wordToCheck) {
+
+//     checkPalindrome = wordToCheck.split('').reverse().join('')
+
+//     if (wordToCheck === checkPalindrome) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+// } console.log(palindrome('arara'))
+
+// SEGUNDO
+
+// let biggestNumber = 0
+// let index = 0
+
+
+// function checkNumbers(arrayNumbers) {
+
+//     for (let number in arrayNumbers) {
+
+//         if (biggestNumber < arrayNumbers[number]) {
+//             biggestNumber = arrayNumbers[number]
+//             index = number
+//         }
+
+
+//     } console.log('O maior índice do Array é: ', index)
+
+//     return checkNumbers
+
+// } checkNumbers([8, 5, 6, 7, 98, 9, 78789, 456, 78, 34, 568, 25, 46])
+
+//TERCEIRO
+
+// let smallestNumber = 0
+// let index = 0
+// let number = 0
+
+// function checkNumbers(arrayNumbers) {
+
+//     smallestNumber = arrayNumbers[number]
+
+//     for (number in arrayNumbers) {
+
+
+//         if (smallestNumber > arrayNumbers[number]) {
+//             smallestNumber = arrayNumbers[number]
+//             index = number
+//         }
+
+//     } console.log('O menor índice no Array é: ', index)
+
+//     return checkNumbers
+
+// } checkNumbers([8, 5, 6, 7, 98, 9, 78789, 167, 78, 34, 568,  25, 3, 46])
+
+// QUARTO
+
+// let charactersCount = ''
+// function nameCount(namesArray) {
+
+//     for (let name in namesArray) {
+
+//         if (charactersCount.length < namesArray[name].length) {
+//             charactersCount = namesArray[name]
+
+//         }
+
+//     } console.log('O nome com maior quantidade de caracteres é: ', charactersCount)
+//     return
+
+// }
+// nameCount(['Alexandre', 'Juliana', 'carmen sandiego', 'Camila', 'Rodolfo', 'Arnold Schwarzenegger'])
+
+//QUINTO
+
+let equalNumber = 0
+let index = 0
+let number = 0
+
+function checkNumbers(arrayNumbers) {
+
+    equalNumber = arrayNumbers[number]
+
+    for (number in arrayNumbers) {
+
+
+        if (equalNumber === arrayNumbers[number]) {
+            smallestNumber = arrayNumbers[number]
+            index = index +1
+        }
+
+    } console.log(index)
+
+    return checkNumbers
+
+} checkNumbers([2,2,5,6,2,4,6,6,6,2])
