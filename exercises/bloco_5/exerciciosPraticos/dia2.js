b1323c394d6de312f84ac5a3c197bf9a0673a659
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-04 15:48:48 -0300

    testes praticos dia 2

---
+++ exercises/bloco_5/exerciciosPraticos/dia2.js
@@ -0,0 +1,11 @@
+let primeiroFilho = document.querySelector('#primeiroFilho')
+let paiDoPai = document.querySelector('#paiDoPai')
+let irmaoAondeVoceEsta = document.createElement('h1')
+
+
+
+primeiroFilho.appendChild(irmaoAondeVoceEsta).innerHTML 
+irmaoAondeVoceEsta.innerHTML = "Testando novas funcionalidades"
+
+
+paiDoPai.removeChild(pai)
