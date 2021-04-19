651e1d67f2df2366c4d230fb51b36ed8953b60b1
Alexandre Oliveira <alexandremolive@gmail.com>
2021-04-19 17:57:54 -0300

    primeiro dia bloco 10 JEST

---
+++ jest.tests/sum.test.js
@@ -0,0 +1,31 @@
+// const sum = (a, b) => a + b;
+
+// test('sums two values', () => {
+//   expect(sum(2, 3)).toEqual(5);
+// });
+
+// test('there is no I in team', () => {
+//   expect('team').not.toMatch(/I/);
+// });
+
+// test('but there is a "stop" in Christoph', () => {
+//   expect('Christoph').toMatch(/stop/);
+// });
+
+const multiplyByTwo = (number) => {
+  if (!number) {
+    throw new Error('number é indefinido')
+  }
+  return number * 2;
+};
+multiplyByTwo(4);
+
+test('testa se multiplyByTwo retorna o resultado da multiplicação', () => {
+  expect(multiplyByTwo(4)).toBe(8);
+});
+test('testa se é lançado um erro quando number é indefinido', () => {
+  expect(() => { multiplyByTwo() }).toThrow();
+});
+test('testa se a mensagem de erro é "number é indefinido"', () => {
+  expect(() => { multiplyByTwo() }).toThrowError(new Error('number é indefinido'));
+});
