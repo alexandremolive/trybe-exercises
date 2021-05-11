167f4d1d938301d6391b758939f6a983e1932c00
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-11 16:49:48 -0300

    melhoramentos nos filtros

--- exercises/Bloco-11-Introducao-a-react/my-pokedex/src/App.js
+++ exercises/Bloco-11-Introducao-a-react/my-pokedex/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Pokedex from './Components/Pokedex'
 import Image from './Components/Image'
+import pokemons from './data'
 
 function App() {
   return (
     <div className="container">
-      < Image source='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1920px-International_Pok%C3%A9mon_logo.svg.png' />
+      < Image  source='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1920px-International_Pok%C3%A9mon_logo.svg.png' />
       <section className='pokedex'>
-        < Pokedex />
+        < Pokedex pokemons={pokemons}/>
 
       </section>
     </div>
--- exercises/Bloco-11-Introducao-a-react/my-pokedex/src/Components/TypeButtons.js
+++ exercises/Bloco-11-Introducao-a-react/my-pokedex/src/Components/TypeButtons.js
@@ -1,13 +1,13 @@
 import React from 'react'
 class TypeButtons extends React.Component {
   render (){
-    const {pokemons, handleTypeBtn} = this.props
+    const {pokemons, handleType} = this.props
     const allPokemons = pokemons.reduce((acc, pokemon) =>  [...acc, pokemon.type], [])
     const pokemonsByType = allPokemons.filter((pokemon, index) => allPokemons.indexOf(pokemon) === index)
     return (
       <div>
       {pokemonsByType.map((pokemon) => 
-        <button onClick={handleTypeBtn} key={pokemon} value={pokemon}>{pokemon}</button>
+        <button onClick={handleType} key={pokemon} value={pokemon}>{pokemon}</button>
       )}
       </div>
     )
